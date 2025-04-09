174957f805d151abbb9fcc1c4f8318eb1fab87f8
EnverKayril <eckayril@gmail.com>
2025-04-09 13:01:32 +0300

    Add project files.

---
+++ AuthServer.Core/Services/IServiceGeneric.cs
@@ -0,0 +1,15 @@
+﻿using SharedLibrary.DTOs;
+using System.Linq.Expressions;
+
+namespace AuthServer.Core.Services
+{
+    public interface IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IEnumerable<TEntity>>> GetAllAsync();
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<Response<TDto>> AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
---
+++ SharedLibrary/DTOs/Response.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.DTOs
+{
+    public class Response<T> where T : class
+    {
+
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccess { get; private set; }
+        public ErrorDTO Error { get; private set; }
+
+        public static Response<T> Success(T data, int statuscode)
+        {
+            return new Response<T> { Data = data, StatusCode = statuscode, IsSuccess = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccess = true };
+        }
+
+        public static Response<T> Fail(ErrorDTO error, int statusCode)
+        {
+            return new Response<T> { Error = error, StatusCode = statusCode, IsSuccess = false };
+        }
+
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow) 
+        {
+            var errorDTO = new ErrorDTO(errorMessage, isShow);
+
+            return new Response<T> { Error = errorDTO, StatusCode = statusCode, IsSuccess = false };
+        }
+    }
+}
